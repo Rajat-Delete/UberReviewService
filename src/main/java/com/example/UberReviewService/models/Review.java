ba02fdb5f5910b5427189beeae7fdae55e488361
Rajat Sharma <rajatxd007@gmail.com>
2024-11-08 00:25:39 +0530

    Added schema code for Review Table

--- src/main/java/com/example/UberReviewService/models/Review.java
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -1,12 +1,33 @@
 package com.example.UberReviewService.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-@Entity
+import java.util.Date;
+
+@Entity //will be used for name at the code level
+@Table(name = "BookingReview") //represent the name at DB Level
 public class Review {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)//this id annotation tells that the id property is a key of our table
     long Id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the format of Date Object to be stored ie. Date/Time/Timestamp
+    @CreatedDate // this annotation tells spring that only handle it for object creation
+    Date createdAt;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) //this annoatation tells spring about format of Date Object to be stored
+    @LastModifiedDate //this annotation tells spring that only handle for object modification
+    Date modifiedAt;
 }
